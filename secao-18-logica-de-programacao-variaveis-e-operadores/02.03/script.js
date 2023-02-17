88b224db6bdf8379fa53b9c5989e92fd81661b62
João Pedro Dias <jp.souza.dias@gmail.com>
2023-02-17 17:57:36 -0300

    script

---
+++ secao-18-logica-de-programacao-variaveis-e-operadores/02.03/script.js
@@ -0,0 +1,10 @@
+//pedir para o usuário digitar dois número
+var n1 = prompt("digite um número");
+n1 = parseFloat(n1);
+
+var n2 = prompt("digite outro número");
+n2 = parseFloat(n2);
+
+var media = (n1 + n2) / 2;
+
+alert("A média é: " + media);
