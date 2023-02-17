75dc5d92d7f3ee46d7770892c8b99a5c18bee553
João Pedro Dias <jp.souza.dias@gmail.com>
2023-02-17 17:25:32 -0300

    script

---
+++ secao-18-logica-de-programacao-variaveis-e-operadores/02.01/script.js
@@ -0,0 +1,6 @@
+/* alert("Olá mundo"); */
+var nome = prompt("Qual seu nome");
+alert("Bem-vindo, " + nome);
+
+var endereco = 1.2; // número decimal / número real
+var estaGravido = false // boolean
