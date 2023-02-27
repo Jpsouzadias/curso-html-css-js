24fb458ef7b2270f76a014d20ab45d0c0bb7a3c5
João Pedro Dias <jp.souza.dias@gmail.com>
2023-02-27 15:50:37 -0300

    readonly

--- secao-25-javascript-dom/06_ex4/script.js
+++ secao-25-javascript-dom/06_ex4/script.js
@@ -1,5 +1,6 @@
-var $txt = document.querySelector('txtNome');
+var $txt = document.querySelector('#txtNome');
 var nome = "João";
 var sobrenome = " Dias";
 
 document.querySelector('#txtNome').value = nome + sobrenome;
+document.querySelector('#txtNome').readOnly = true;
