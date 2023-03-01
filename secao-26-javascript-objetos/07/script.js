93a405d63934a9c27e97fb807b3f33180bb86d97
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-01 16:29:04 -0300

    objetos-customizados-propriedas-metodos

--- secao-26-javascript-objetos/07/script.js
+++ secao-26-javascript-objetos/07/script.js
@@ -21,3 +21,28 @@ function mudaN(n){
 }
 mudaN(n);
 console.log(n);
+
+console.log('-----------------')
+
+function mudarCor(cor){
+    console.log(this);
+    this.cor = cor;
+}
+
+var caneta1 = {
+    cor: 'red',
+    preco: 1,
+    mudarCor: mudarCor
+}
+
+var caneta2 = {
+    cor: 'blue',
+    preco: 2,
+    mudarCor: mudarCor
+}
+
+caneta1.mudarCor('green');
+console.log(caneta1);
+console.log(caneta2);
+
+var caneta3 = new caneta1();
