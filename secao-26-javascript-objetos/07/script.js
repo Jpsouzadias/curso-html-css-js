1aaa92cd0f118be8d921105fe31fad174063c216
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-01 15:05:23 -0300

    construtores-operador-new-objetos-por-referencia

--- secao-26-javascript-objetos/07/script.js
+++ secao-26-javascript-objetos/07/script.js
@@ -1,1 +1,23 @@
 var minhaStr = "João";
+
+var hoje = new Date();
+
+var x = [0];
+
+function mudaArr(x){
+    var y = x;
+    y.push(2);
+    y.push(3);
+}
+
+mudaArr(x);
+
+console.log('--------');
+
+var n = 0;
+function mudaN(n){
+    var n2 = n;
+    console.log(++n2);
+}
+mudaN(n);
+console.log(n);
