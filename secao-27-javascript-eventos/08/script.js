32a64d357960089458ae26aaf79422e60012e1f2
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-02 17:21:27 -0300

    delegacao-de-eventos

--- secao-27-javascript-eventos/08/script.js
+++ secao-27-javascript-eventos/08/script.js
@@ -46,3 +46,27 @@ document.addEventListener('click', function(e){
     console.log(e.target);
     console.log(this); 
 }, false);
+
+var $ul = document.querySelector('ul');
+var $lis = document.querySelectorAll('li');
+
+/* var x = 0;
+while ($lis[x]){
+    $lis[x].addEventListener('click', function(e){
+        e.stopPropagation();
+        console.log('clicou na li');
+    })
+    x++;
+} */
+
+$ul.addEventListener('click', function(e){
+    e.stopPropagation();
+    console.log(this);
+    console.log(e.target);
+    console.log(e.currentTarget)
+
+    console.log(e.target.nodeName);
+    if(e.target.nodeName === 'LI'){
+        alert('clicou na LI')
+    }
+})
