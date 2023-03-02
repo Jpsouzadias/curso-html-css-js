b1b9a93b2545ee9acbd02fdf86d1209212ff9950
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-02 16:33:58 -0300

    propagacao-de-eventos

--- secao-27-javascript-eventos/08/script.js
+++ secao-27-javascript-eventos/08/script.js
@@ -1,4 +1,4 @@
-var btn2 = document.getElementById('btn2');
+/* var btn2 = document.getElementById('btn2');
 var btn3 = document.getElementById('btn3')
 
 btn2.onclick = clicar();
@@ -25,4 +25,19 @@ btn3.onclick= function () {
 
 btn3.onclick= function () {
     alert('mundo');
-}
+} */
+
+var $d1 = document.querySelector('#d1');
+var $d1Span = document.querySelector('#d1 span');
+
+$d1.addEventListener('click', function () {
+    console.log('click no $d1');    
+}, true);
+
+$d1Span.addEventListener('click', function () {
+    console.log('click no $d1Span');    
+}, true);
+
+document.addEventListener('click', function () {
+    console.log('clicou no document');
+}, true);
