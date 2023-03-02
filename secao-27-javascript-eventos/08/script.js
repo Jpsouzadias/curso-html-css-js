3eda9d51eb943f02b38912722d33f07164307cf8
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-02 16:15:22 -0300

    dom-level-2

--- secao-27-javascript-eventos/08/script.js
+++ secao-27-javascript-eventos/08/script.js
@@ -1,4 +1,5 @@
 var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3')
 
 btn2.onclick = clicar();
 
@@ -8,4 +9,20 @@ function clicar() {
     return function() {
         alert('clicou2');
     }
+}
+
+btn3.addEventListener('click', function(){
+    alert('clicou com DOM level 2');
+})
+
+btn3.addEventListener('click', function(){
+    alert('clicou com DOM level 2 com outro evento');
+})
+
+btn3.onclick= function () {
+    alert('ola');
+}
+
+btn3.onclick= function () {
+    alert('mundo');
 }
