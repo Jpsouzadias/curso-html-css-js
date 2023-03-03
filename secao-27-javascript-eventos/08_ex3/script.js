7a1e9281f6a6ca703e910414507b46a12e41ff31
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-03 15:53:57 -0300

    contador-de-caracteres-de-um-textarea-parte-4

--- secao-27-javascript-eventos/08_ex3/script.js
+++ secao-27-javascript-eventos/08_ex3/script.js
@@ -1,11 +1,14 @@
 (function(){
     'use strict';
-    var maxima = 100;
 
     var $txtMsg = document.getElementById('msg');
     var $resta = document.getElementById('resta');
+    var $restaContainer = document.getElementById('restaContainer');
+    var maxima = $txtMsg.maxLength;
 
     $resta.textContent = maxima;
+
+    $restaContainer.style.display = 'block';
 
     $txtMsg.addEventListener('input', checkLength);
 
