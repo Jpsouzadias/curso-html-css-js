8fe9a08db2d24bb7594cebc83e1e1ae5e7ff7ffc
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-06 14:41:38 -0300

    impedir-envio
    Impedir o envio de formulário sem aceitar os termos de contrato.

--- secao-27-javascript-eventos/08_ex4/script.js
+++ secao-27-javascript-eventos/08_ex4/script.js
@@ -1,3 +1,23 @@
 (function(){
     'use strict';
+
+    var $btn = document.getElementById('btn');
+    var $chk = document.getElementById('chk');
+
+    toggleBtn();
+
+    $chk.addEventListener('change', function(){
+        
+        /* if (this.checked){
+            $btn.disabled = false;
+        } else {
+            $btn.disabled = true;
+        } */
+
+        toggleBtn();
+    });
+
+    function toggleBtn(){
+       $btn.disabled = !$chk.checked;
+    }
 })()
