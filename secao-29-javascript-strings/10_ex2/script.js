fc29fdfb20c8ff77582c4ea06d0fa674ec3e5d6d
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-08 17:15:06 -0300

    resolucao-exercicio

---
+++ secao-29-javascript-strings/10_ex2/script.js
@@ -0,0 +1,22 @@
+(function (){
+    'use strick';
+    window.addEventListener('load', init);
+    
+    function init(){
+        var $paragrafos = document.querySelectorAll('p');
+
+        var x = 0;
+        while($paragrafos[x]) {
+            $paragrafos[x].innerHTML = formatarTexto($paragrafos[x].innerHTML);
+            x++;
+        }
+    }
+
+    function formatarTexto(texto){
+        var numeroMaximoCaracteres = 200;
+        if(texto.length <= numeroMaximoCaracteres) 
+            return texto;
+        var subTexto = texto.slice(0, numeroMaximoCaracteres);
+        return subTexto + '...';    
+    }
+})()
