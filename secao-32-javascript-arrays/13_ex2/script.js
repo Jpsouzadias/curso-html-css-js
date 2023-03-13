ad4bb2d7598f10ed0b4436b1178e07d25639302a
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-13 18:26:23 -0300

    desafio

---
+++ secao-32-javascript-arrays/13_ex2/script.js
@@ -0,0 +1,9 @@
+var paragrafos = document.querySelectorAll('p');
+
+for (var i = o; i < paragrafos.length; i++){
+    paragrafos[i].innerHTML = formataParagrafo(paragrafos[i].innerHTML);
+}
+
+function formataParagrafo(texto){
+    
+}
