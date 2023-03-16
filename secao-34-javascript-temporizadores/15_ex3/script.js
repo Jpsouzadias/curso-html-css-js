821ef2268a2937c411f4e46b118394e9a55d8fb0
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-16 17:09:44 -0300

    retirando-link

--- secao-34-javascript-temporizadores/15_ex3/script.js
+++ secao-34-javascript-temporizadores/15_ex3/script.js
@@ -1,9 +1,34 @@
 (function() {
     
     var $cronometro = document.querySelector('#cronometro');
-    var $inicar = document.querySelector('#iniciar');
+    var $iniciar = document.querySelector('#iniciar');
     var $pausar = document.querySelector('#pausar');
     var $parar = document.querySelector('#parar');
 
-    
+    $iniciar.addEventListener('click', iniciar);
+    $pausar.addEventListener('click', pausar);
+    $parar.addEventListener('click', parar);
+
+    var msClickIniciar = 0;
+
+    function iniciar() {
+        msClickIniciar = Date.now();
+        iniciarCronometo();
+    }
+
+    function iniciarCronometo() {
+        setInterval(function () {
+            var msAgora = Date.now();
+            var diferença = msAgora - msClickIniciar;
+            $cronometro.value = diferença;
+        }, 100);
+    }
+
+    function pausar() {
+        
+    }
+
+    function parar() {
+        
+    }
 })()
