7a7442f6bac94dcf0d209c79e4a0c3cfba10edfa
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-16 17:28:24 -0300

    resolucao-pt-2

--- secao-34-javascript-temporizadores/15_ex3/script.js
+++ secao-34-javascript-temporizadores/15_ex3/script.js
@@ -20,8 +20,33 @@
         setInterval(function () {
             var msAgora = Date.now();
             var diferença = msAgora - msClickIniciar;
-            $cronometro.value = diferença;
+            $cronometro.value = formataMs(diferença);
         }, 100);
+    }
+
+    function formataMs(ms){
+        /* 
+        ms = 900 => 900
+        ms = 1500 => 1:500
+        ms = 59000 => 59:000
+        ms = 61500 => 1:1:500 
+        */
+
+        if (ms < 1000) {
+            return ms;
+        } else if(ms < 60000){ // 30500
+            var s = ms/1000;
+            s = parseInt(s); // 30
+            var c = ms - ( s * 1000); //30500 -  30  * 1000 = 500
+            return s + ':' + c;
+        } else {
+            var m = ms / ( 60000 );
+            m = parseInt(m); // 1
+            var s = ms / 1000 - ( m * 60 ); //61.5 = 1.5
+            s = parseInt(s); // 1
+            var c = ms - (s * 1000) - ( m * 60 * 1000 );
+            return m + ':' + s + ':' + c;
+        }
     }
 
     function pausar() {
