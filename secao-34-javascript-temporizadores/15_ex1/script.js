4313b1a276370b09846e51e2b7eb76e8fd32fd42
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-16 15:11:55 -0300

    banner-popup

--- secao-34-javascript-temporizadores/15_ex1/script.js
+++ secao-34-javascript-temporizadores/15_ex1/script.js
@@ -4,15 +4,21 @@
 
     $bannerBtn.addEventListener('click', escondeBanner);
 
+    var intervaloEsconde = null;
+
     function mostraBanner() {
         $banner.style.display = 'block';
+
+        intervaloEsconde = setTimeout(escondeBanner, 3000);
     }
     function escondeBanner() {
         $bannerBtn.removeEventListener('click', escondeBanner);
         $banner.parentNode.removeChild($banner);
+
+        clearTimeout(intervaloEsconde);
     }
 
-    setTimeout(mostraBanner, 2000);
+    setTimeout(mostraBanner, 3000);
 
-    setTimeout(escondeBanner, 4000);
+    
 })()
