5af0f9a0a2efc5b672dea1329917e42248ffd171
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-16 16:39:40 -0300

    cronometro

---
+++ secao-34-javascript-temporizadores/15_ex3/script.js
@@ -0,0 +1,9 @@
+(function() {
+    
+    var $cronometro = document.querySelector('#cronometro');
+    var $inicar = document.querySelector('#iniciar');
+    var $pausar = document.querySelector('#pausar');
+    var $parar = document.querySelector('#parar');
+
+    
+})()
