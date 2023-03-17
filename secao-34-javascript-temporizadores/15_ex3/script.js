8392158af94d6bfe8b0e9aa702c3f3e81082e4b9
João Pedro Dias <jp.souza.dias@gmail.com>
2023-03-17 16:50:35 -0300

    resolucao-parte-6-refatoracao

--- secao-34-javascript-temporizadores/15_ex3/script.js
+++ secao-34-javascript-temporizadores/15_ex3/script.js
@@ -25,15 +25,14 @@
     function pausar() {
         if(!$cronometro.value) return;
         if (rodando) {
-            rodando = false;
             clearInterval(intervalo);
             msClickPausar = Date.now();
             msTempoDecorrido += (msClickPausar - msClickIniciar);
         } else {
-            rodando = true;
             msClickIniciar = Date.now();
             iniciarCronometro(msTempoDecorrido);
         }
+        rodando = !rodando;
     }
 
     function parar() {
@@ -55,28 +54,18 @@
     }
 
     function formataMs(ms){
-        /* 
-        ms = 900 => 900
-        ms = 1500 => 1:500
-        ms = 59000 => 59:000
-        ms = 61500 => 1:1:500 
-        */
-
+        const MINUTO = 60 * 1000;
         if (ms < 1000) {
             return ms;
-        } else if(ms < 60000){ // 30500
+        } else if(ms < MINUTO){ 
             var s = ms/1000;
-            s = parseInt(s); // 30
-            var c = ms - ( s * 1000); //30500 -  30  * 1000 = 500
+            s = parseInt(s); 
+            var c = ms - ( s * 1000); 
             return s + ':' + c;
         } else {
-            var m = ms / ( 60000 );
-            m = parseInt(m); // 1
-            var s = ms / 1000 - ( m * 60 ); //61.5 = 1.5
-            s = parseInt(s); // 1
-            var c = ms - (s * 1000) - ( m * 60 * 1000 );
-            return m + ':' + s + ':' + c;
+            var m = ms / ( MINUTO );
+            m = parseInt(m); 
+            return m + ':' + formataMs( ms - m * MINUTO );
         }
     }
-
 })()
